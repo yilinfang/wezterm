b18bf36b6b43d4d25f5e3f59e78eb9dff05df342
Yilin Fang <qzfyl98@outlook.com>
2025-02-23 10:20:25 -0500

    Small cleanup

--- wezterm.lua
+++ wezterm.lua
@@ -1,20 +1,21 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "iTerm2 Tango Dark"
-
 config.front_end = "WebGpu"
+
+config.color_scheme = "iTerm2 Tango Dark"
 
 config.font = wezterm.font("VictorMono Nerd Font")
 config.font_size = 14.0
 
 config.initial_rows = 30
 config.initial_cols = 120
+
+config.enable_scroll_bar = true
+
 config.window_frame = {
 	font_size = 14.0,
 }
-
-config.enable_scroll_bar = true
 
 config.window_padding = {
 	left = 15,
