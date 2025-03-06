66fb0a211ba7440c9520dc7ca87c2fd3da9fe361
Yilin Fang <qzfyl98@outlook.com>
2025-03-06 18:36:57 -0500

    Update fonts

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,11 @@ config.front_end = "WebGpu"
 config.color_scheme = "iTerm2 Tango Dark"
 
 config.font_size = 15.0
-config.font = wezterm.font("CaskaydiaMono Nerd Font")
+config.font = wezterm.font_with_fallback({
+	"Cascadia Code",
+	"Symbols Nerd Font",
+})
+
 config.initial_rows = 40
 config.initial_cols = 120
 
